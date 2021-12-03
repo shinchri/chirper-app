f67bfb7b1873548c5ed93d297a2bdd7fa21c7bfc
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-12-03 14:29:04 -0600

    Add Comments on some resources

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -7,34 +7,38 @@ export default function tweets (state={}, action) {
                 ...state,
                 ...action.tweets
             }
-            case TOGGLE_TWEET:
-                return {
-                    ...state,
-                    [action.id]: {
-                        ...state[action.id],
-                        likes: action.hasLiked === true
-                            ? state[action.id].likes.filter((uid) => uid !== action.authedUser)
-                            : state[action.id].likes.concat([action.authedUser])
+        case TOGGLE_TWEET:
+            return {
+                ...state,
+                [action.id]: {
+                    ...state[action.id],
+                    likes: action.hasLiked === true
+                        ? state[action.id].likes.filter((uid) => uid !== action.authedUser)
+                        : state[action.id].likes.concat([action.authedUser])
+                }
+            }
+        // Immutable update patterns
+        // https://redux.js.org/usage/structuring-reducers/immutable-update-patterns
+        // Designing the State Shape
+        // https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers#designing-the-state-shape
+        case ADD_TWEET:
+            const { tweet } = action
+
+            let replyingTo = {}
+            if (tweet.replyingTo !== null) {
+                replyingTo = {
+                    [tweet.replyingTo]: {
+                        ...state[tweet.replyingTo],
+                        replies: state[tweet.replyingTo].replies.concat([tweet.id])
                     }
                 }
-            case ADD_TWEET:
-                const { tweet } = action
+            }
 
-                let replyingTo = {}
-                if (tweet.replyingTo !== null) {
-                    replyingTo = {
-                        [tweet.replyingTo]: {
-                            ...state[tweet.replyingTo],
-                            replies: state[tweet.replyingTo].replies.concat([tweet.id])
-                        }
-                    }
-                }
-
-                return {
-                    ...state,
-                    [action.tweet.id]: action.tweet,
-                    ...replyingTo,
-                }
+            return {
+                ...state,
+                [action.tweet.id]: action.tweet,
+                ...replyingTo,
+            }
         default:
             return state
     }
